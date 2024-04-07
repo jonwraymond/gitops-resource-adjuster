797cb629c6a1071fd3911cdb1369e0bebd6e3abe
Jonathan <jraymond@itsecurityworks.net>
2024-04-07 12:44:32 -0600

    Update VPA fetcher to use dynamic client for recommendations

--- internal/vpa/fetcher.go
+++ internal/vpa/fetcher.go
@@ -1,31 +1,35 @@
 package vpa
 
 import (
-	"context"
-	"fmt"
-
-	autoscalingv1 "k8s.io/autoscaler/vertical-pod-autoscaler/pkg/apis/autoscaling.k8s.io/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
+    "context"
+    "fmt"
+    "k8s.io/apimachinery/pkg/apis/meta/v1"
+    "k8s.io/apimachinery/pkg/runtime/schema"
+    "k8s.io/client-go/dynamic"
+    "k8s.io/client-go/rest"
 )
 
-func FetchRecommendations(vpaName, namespace string) (*autoscalingv1.VerticalPodAutoscaler, error) {
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		return nil, fmt.Errorf("failed to get in-cluster config: %w", err)
-	}
+func FetchRecommendations(vpaName, namespace string) (*unstructured.Unstructured, error) {
+    config, err := rest.InClusterConfig()
+    if err != nil {
+        return nil, fmt.Errorf("failed to get in-cluster config: %w", err)
+    }
 
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create clientset: %w", err)
-	}
+    dynamicClient, err := dynamic.NewForConfig(config)
+    if err != nil {
+        return nil, fmt.Errorf("failed to create dynamic client: %w", err)
+    }
 
-	vpaClient := clientset.AutoscalingV1().VerticalPodAutoscalers(namespace)
-	vpa, err := vpaClient.Get(context.TODO(), vpaName, metav1.GetOptions{})
-	if err != nil {
-		return nil, fmt.Errorf("failed to fetch VPA recommendations: %w", err)
-	}
+    vpaResource := schema.GroupVersionResource{
+        Group:    "autoscaling.k8s.io",
+        Version:  "v1",
+        Resource: "verticalpodautoscalers",
+    }
 
-	return vpa, nil
+    vpa, err := dynamicClient.Resource(vpaResource).Namespace(namespace).Get(context.TODO(), vpaName, v1.GetOptions{})
+    if err != nil {
+        return nil, fmt.Errorf("failed to fetch VPA recommendations: %w", err)
+    }
+
+    return vpa, nil
 }
